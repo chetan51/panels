3e68471d5b56ab640243dac4bdd080e181b8c32e
Chetan Surpur <chetan51@gmail.com>
2011-08-22 17:49:39 -0700

    Redeploy

--- bookmarklet.js
+++ bookmarklet.js
@@ -14,9 +14,9 @@
     hn_comments = $(hn_comments_html);
     hn_status = hn_comments.find("#hn-status");
     hn_frame = hn_comments.find("#hn-frame");
+    hn_comments.appendTo("body");
+    hn_comments.slideDown("fast");
     $.getJSON("http://api.ihackernews.com/getid?format=jsonp&callback=?&url=" + encodeURI(window.location), function(data) {
-      hn_comments.appendTo("body");
-      hn_comments.slideDown("fast");
       if (data.length) {
         hn_frame.attr("src", "http://news.ycombinator.com/item?id=" + data[0]);
       } else {
